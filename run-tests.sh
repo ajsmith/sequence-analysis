560435aaf30d36f78d859bb6cacd14eeb9f76343
Alexander Smith <ajsmith@acm.org>
2021-09-24 00:32:25 -0400

    Start pairwise alignment module.

--- run-tests.sh
+++ run-tests.sh
@@ -2,5 +2,11 @@
 #
 # Run tests and code quality checks.
 
+set -e
+
+cd $(dirname $0)
+
+source venv/bin/activate
+mypy --strict src
 pytest .
 pylint src/ tests/
---
+++ src/align/pairwise.py
@@ -0,0 +1,7 @@
+"""
+Pairwise alignment functions.
+
+"""
+
+def needleman_wunsch(sequence1: str, sequence2: str) -> list[str]:
+    return []
