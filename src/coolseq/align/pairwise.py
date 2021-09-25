df948ab87d4e716faf04f53706056cfe58e9b8de
Alexander Smith <ajsmith@acm.org>
2021-09-25 16:28:30 -0400

    Decouple matrix initialization from match/mismatch variables.

--- src/coolseq/align/pairwise.py
+++ src/coolseq/align/pairwise.py
@@ -26,9 +26,57 @@ ARROW_CHAR_MAP = {
     L_ARROW: '←',
 }
 
-def needleman_wunsch(sequence1: str, sequence2: str, match: int=1, mismatch: int=-1, gap: int=-1) -> list[str]:
+
+class SimilarityScorer:
+    """Determine similarity scores for a cell in an alignment score matrix.
+
+    """
+
+
+    def __init__(self, match: int=1, mismatch: int=-1, gap_penalty: int=-1) -> None:
+        """Instantiate a scorer."""
+        self.match = match
+        self.mismatch = mismatch
+        self.gap_penalty = gap_penalty
+
+    def score(self, scores: ScoreMatrix, sequence1: str, sequence2: str, i: int, j: int) -> tuple[Score, Arrow]:
+        """Determine score and arrow values for a cell position."""
+        cell = {}
+        # The diagonal score is the diagonal neighbor plus the
+        # match/mismatch score
+        top_left = scores[i-1][j-1] + self.match_score(i, j, sequence1, sequence2)
+        cell[top_left] = D_ARROW
+        # The top score is the top neighbor plus the gap penalty.
+        top = scores[i-1][j] + self.gap_penalty
+        cell[top] = T_ARROW
+        # The left score is the left neighbor plus the gap penalty.
+        left = scores[i][j-1] + self.gap_penalty
+        cell[left] = L_ARROW
+        # Final score is the max of the top-left, top, and left
+        # values.
+        final_score = max(top_left, top, left)
+        # The arrow points in the direction of the neighbor from where
+        # the best score came. This implementation doesn't include
+        # branches, but could with a little extra effort.
+        arrow = cell[final_score]
+        return (final_score, arrow)
+
+
+    def match_score(self, i: int, j: int, seq1: str, seq2: str) -> int:
+        """Return the match score for a position."""
+        if is_match(i, j, seq1, seq2):
+            score = self.match
+        else:
+            score = self.mismatch
+        return score
+
+
+def needleman_wunsch(
+        sequence1: str, sequence2: str, match: int=1, mismatch: int=-1, gap: int=-1,
+    ) -> list[str]:
     """Return the pairwise alignment found using Needleman-Wunsch."""
-    (scores, arrows) = initialize_matrix(sequence1, sequence2, match, mismatch, gap)
+    scorer = SimilarityScorer(match, mismatch, gap)
+    (scores, arrows) = initialize_matrix(sequence1, sequence2, scorer)
     path = list(trace_path(arrows))
     alignment = build_alignment(sequence1, sequence2, path)
     # score = compute_alignment_score(scores, path)
@@ -36,13 +84,14 @@ def needleman_wunsch(sequence1: str, sequence2: str, match: int=1, mismatch: int
 
 
 def initialize_matrix(
-        sequence1: str, sequence2: str, match: int, mismatch: int, gap: int
+        sequence1: str, sequence2: str, scorer: SimilarityScorer,
     ) -> tuple[ScoreMatrix, ArrowMatrix]:
     """Return the initialized matrix.
 
     Here's a small example:
 
-    >>> scores, arrows = initialize_matrix('at', 'aagt', 1, -1, -1)
+    >>> scores, arrows = initialize_matrix(
+    ...    'at', 'aagt', SimilarityScorer(1, -1, -1))
     >>> print_matrix(scores)
     [0, -1, -2, -3, -4]
     [-1, 1,  0, -1, -2]
@@ -54,7 +103,8 @@ def initialize_matrix(
 
     Here's a bigger example taken from the Needleman-Wunsch Wikipedia page:
 
-    >>> scores, arrows = initialize_matrix('gattaca', 'gcatgcu', 1, -1, -1)
+    >>> scores, arrows = initialize_matrix(
+    ...     'gattaca', 'gcatgcu', SimilarityScorer(1, -1, -1))
     >>> print_matrix(scores)
     [0,  -1, -2, -3, -4, -5, -6, -7]
     [-1,  1,  0, -1, -2, -3, -4, -5]
@@ -77,12 +127,11 @@ def initialize_matrix(
     """
     n = len(sequence1)
     m = len(sequence2)
-    (scores, arrows) = initialize_matrix_top(m, gap)
-    scorer = SimilarityScorer(match, mismatch, gap)
+    (scores, arrows) = initialize_matrix_top(m, scorer.gap_penalty)
     for i in range(1, n + 1):
         # The left-most score is simply the score directly above plus
         # the gap penalty.
-        scores.append([scores[i-1][0] + gap])
+        scores.append([scores[i-1][0] + scorer.gap_penalty])
         # The left-most arrow always points up.
         arrows.append([T_ARROW])
         for j in range(1, m + 1):
@@ -95,8 +144,8 @@ def initialize_matrix(
 def initialize_matrix_top(m_len: int, gap: int) -> tuple[ScoreMatrix, ArrowMatrix]:
     """Return a matrix with only its top initialized.
 
-    `m_length` is the sequence length (of the 2nd sequence). `gap` is
-    the gap penalty.
+    `m_len` is the sequence length (of the 2nd sequence). `gap` is the
+    gap penalty.
 
     For example, if the sequence length is 4 and the gap penalty is
     -1, then the top of the matrix looks like:
@@ -141,7 +190,8 @@ def trace_path(matrix: ArrowMatrix) -> Iterable[tuple[int, int, int]]:
 
     Here's a small example:
 
-        >>> scores, arrows = initialize_matrix('at', 'aagt', 1, -1, -1)
+        >>> scores, arrows = initialize_matrix(
+        ...     'at', 'aagt', SimilarityScorer(1, -1, -1))
         >>> path = trace_path(arrows)
         >>> print(list(path))
         [(2, 4, 1), (1, 3, 3), (1, 2, 3), (1, 1, 1), (0, 0, 0)]
@@ -166,7 +216,7 @@ def trace_path(matrix: ArrowMatrix) -> Iterable[tuple[int, int, int]]:
 def build_alignment(sequence1: str, sequence2: str, path: Sequence[tuple[int, int, int]]) -> list[str]:
     """Align two sequences from the arrow path.
 
-    >>> _, arrows = initialize_matrix('at', 'aagt', 1, -1, -1)
+    >>> _, arrows = initialize_matrix('at', 'aagt', SimilarityScorer(1, -1, -1))
     >>> path = list(trace_path(arrows))
     >>> alignment = build_alignment('at', 'aagt', path)
     >>> print_alignment(alignment)
@@ -210,47 +260,3 @@ def print_arrow_matrix(matrix: ArrowMatrix) -> None:
     for row in matrix:
         arrow_line = ' '.join(ARROW_CHAR_MAP[e] for e in row)
         print('[' + arrow_line + ']')
-
-
-class SimilarityScorer:
-    """Determine similarity scores for a cell in an alignment score matrix.
-
-    """
-
-
-    def __init__(self, match: int=1, mismatch: int=-1, gap: int=-1) -> None:
-        """Instantiate a scorer."""
-        self.match = match
-        self.mismatch = mismatch
-        self.gap = gap
-
-    def score(self, scores: ScoreMatrix, sequence1: str, sequence2: str, i: int, j: int) -> tuple[Score, Arrow]:
-        """Determine score and arrow values for a cell position."""
-        cell = {}
-        # The diagonal score is the diagonal neighbor plus the
-        # match/mismatch score
-        top_left = scores[i-1][j-1] + self.match_score(i, j, sequence1, sequence2)
-        cell[top_left] = D_ARROW
-        # The top score is the top neighbor plus the gap penalty.
-        top = scores[i-1][j] + self.gap
-        cell[top] = T_ARROW
-        # The left score is the left neighbor plus the gap penalty.
-        left = scores[i][j-1] + self.gap
-        cell[left] = L_ARROW
-        # Final score is the max of the top-left, top, and left
-        # values.
-        final_score = max(top_left, top, left)
-        # The arrow points in the direction of the neighbor from where
-        # the best score came. This implementation doesn't include
-        # branches, but could with a little extra effort.
-        arrow = cell[final_score]
-        return (final_score, arrow)
-
-
-    def match_score(self, i: int, j: int, seq1: str, seq2: str) -> int:
-        """Return the match score for a position."""
-        if is_match(i, j, seq1, seq2):
-            score = self.match
-        else:
-            score = self.mismatch
-        return score
