e1248f1c031ccdf7fd9f0c698d47965b257c338c
Alexander Smith <ajsmith@acm.org>
2021-09-27 01:34:45 -0400

    Implement Waterman-Smith-Beyer algorithm.

--- src/coolseq/align/pairwise.py
+++ src/coolseq/align/pairwise.py
@@ -1,16 +1,19 @@
 """Pairwise alignment functions.
 
 """
+from abc import ABC, abstractmethod
 from collections.abc import Iterable, Sequence
 from itertools import islice
+from typing import Optional
 
 
-# Type definitions for score and arrow matrices
+# Type definitions
 Score = int
 Arrow = int
 Matrix = list[list[int]]
 ScoreMatrix = list[list[Score]]
 ArrowMatrix = list[list[Arrow]]
+ScoringOptions = dict[str, int]
 
 
 # Arrow value constants
@@ -27,27 +30,53 @@ ARROW_CHAR_MAP = {
 }
 
 
-class SimilarityScorer:
+class Scorer(ABC):
+    """Abstract scorer"""
+
+    match = 1
+    mismatch = -1
+    gap_extend = -1
+
+    @abstractmethod
+    def get_top_edges(self, width: int) -> tuple[ScoreMatrix, ArrowMatrix]:
+        """Fill the top edge of the score and arrow matrices."""
+
+    @abstractmethod
+    def fill_next_row(
+            self, scores: ScoreMatrix, arrows: ArrowMatrix, i: int, width: int, sequence1: str, sequence2: str
+        ) -> None:
+        """Generate the next row of scores and arrows."""
+
+    def match_score(self, i: int, j: int, seq1: str, seq2: str) -> int:
+        """Return the match score for a position."""
+        if is_match(i, j, seq1, seq2):
+            score = self.match
+        else:
+            score = self.mismatch
+        return score
+
+
+class NWScorer(Scorer):
     """Determine similarity scores for a cell in an alignment score matrix.
 
     """
 
-    def __init__(self, match: int=1, mismatch: int=-1, gap_penalty: int=-1) -> None:
+    def __init__(self, match: int=1, mismatch: int=-1, gap_extend: int=-1) -> None:
         """Instantiate a scorer."""
         self.match = match
         self.mismatch = mismatch
-        self.gap_penalty = gap_penalty
+        self.gap_extend = gap_extend
 
     def get_top_edges(self, width: int) -> tuple[ScoreMatrix, ArrowMatrix]:
         """Fill the top edge of the score and arrow matrices.
 
-        >>> scorer = SimilarityScorer(1, -1, -1)
+        >>> scorer = NWScorer(1, -1, -1)
         >>> scores, arrows = scorer.get_top_edges(5)
         >>> scores
         [[0, -1, -2, -3, -4]]
         >>> arrows
         [[0, 3, 3, 3, 3]]
-        >>> scorer = SimilarityScorer(1, -1, -2)
+        >>> scorer = NWScorer(1, -1, -2)
         >>> scores, arrows = scorer.get_top_edges(4)
         >>> scores
         [[0, -2, -4, -6]]
@@ -66,7 +95,7 @@ class SimilarityScorer:
             # The next score is simply the previous score on the left
             # plus the gap penalty, and the next arrow always points
             # left.
-            score += self.gap_penalty
+            score += self.gap_extend
             scores_top.append(score)
             arrows_top.append(L_ARROW)
         return ([scores_top], [arrows_top])
@@ -77,7 +106,7 @@ class SimilarityScorer:
         """Generate the next row of scores and arrows."""
         # The left-most score is simply the score directly above plus
         # the gap penalty.
-        score = scores[i-1][0] + self.gap_penalty
+        score = scores[i-1][0] + self.gap_extend
         # The left-most arrow always points up.
         arrow = T_ARROW
         # Append new lists containing the initial values to the scores
@@ -97,10 +126,10 @@ class SimilarityScorer:
         top_left = scores[i-1][j-1] + self.match_score(i, j, sequence1, sequence2)
         cell[top_left] = D_ARROW
         # The top score is the top neighbor plus the gap penalty.
-        top = scores[i-1][j] + self.gap_penalty
+        top = scores[i-1][j] + self.gap_extend
         cell[top] = T_ARROW
         # The left score is the left neighbor plus the gap penalty.
-        left = scores[i][j-1] + self.gap_penalty
+        left = scores[i][j-1] + self.gap_extend
         cell[left] = L_ARROW
         # Final score is the max of the top-left, top, and left
         # values.
@@ -111,36 +140,150 @@ class SimilarityScorer:
         arrow = cell[final_score]
         return (final_score, arrow)
 
-    def match_score(self, i: int, j: int, seq1: str, seq2: str) -> int:
-        """Return the match score for a position."""
-        if is_match(i, j, seq1, seq2):
-            score = self.match
-        else:
-            score = self.mismatch
-        return score
+
+class WSBScorer(Scorer):
+    """Distance scorer for Smith-Waterman-Beyer alignments.
+
+    """
+
+    def __init__(self, options: Optional[ScoringOptions] = None):
+        opts = self.default_options
+        if options:
+            opts.update(options)
+        self.match = opts['match']
+        self.mismatch = opts['mismatch']
+        self.gap_start = opts['gap_start']
+        self.gap_extend = opts['gap_extend']
+        self.options = opts
 
 
-def needleman_wunsch(
+    @property
+    def default_options(self) -> dict[str, int]:
+        """Return default scoring options."""
+        return {
+            'match': 0,
+            'mismatch': 1,
+            'gap_start': 1,
+            'gap_extend': 1,
+        }
+
+
+    def get_top_edges(self, width: int) -> tuple[ScoreMatrix, ArrowMatrix]:
+        """Fill the top edge of the score and arrow matrices.
+
+        >>> scorer = WSBScorer()
+        >>> scorer.gap_extend
+        1
+        >>> scores, arrows = scorer.get_top_edges(5)
+        >>> scores
+        [[0, 2, 3, 4, 5]]
+        >>> arrows
+        [[0, 3, 3, 3, 3]]
+        >>> scorer = WSBScorer({'gap_extend': 2})
+        >>> scores, arrows = scorer.get_top_edges(4)
+        >>> scores
+        [[0, 3, 5, 7]]
+        >>> arrows
+        [[0, 3, 3, 3]]
+
+        """
+        scores : ScoreMatrix = [[]]
+        arrows : ArrowMatrix = [[]]
+        # The top-left score is always zero and the top-left arrow is
+        # always the stop arrow.
+        score = 0
+        scores[0].append(score)
+        arrows[0].append(S_ARROW)
+        for j in range(1, width):
+            score = self.best_gap_left(scores, 0, j)
+            scores[0].append(score)
+            arrows[0].append(L_ARROW)
+        return (scores, arrows)
+
+    def fill_next_row(
+            self, scores: ScoreMatrix, arrows: ArrowMatrix, i: int, width: int, sequence1: str, sequence2: str
+        ) -> None:
+        """Generate the next row of scores and arrows."""
+        # The left-most score is simply the score directly above plus
+        # the gap penalty.
+        score = self.best_gap_top(scores, i, 0)
+        # The left-most arrow always points up.
+        arrow = T_ARROW
+        # Append new lists containing the initial values to the scores
+        # and arrows matrices.
+        scores.append([score])
+        arrows.append([arrow])
+        for j in range(1, width):
+            score, arrow = self.score(scores, sequence1, sequence2, i, j)
+            scores[i].append(score)
+            arrows[i].append(arrow)
+
+    def score(self, scores: ScoreMatrix, sequence1: str, sequence2: str, i: int, j: int) -> tuple[Score, Arrow]:
+        """Calculate score and arrow values for a cell position."""
+        cell = {}
+        # The diagonal score is the diagonal neighbor plus the
+        # match/mismatch score
+        top_left = scores[i-1][j-1] + self.match_score(i, j, sequence1, sequence2)
+        cell[top_left] = D_ARROW
+        # The top score is the top neighbor plus the gap penalty.
+        top = self.best_gap_top(scores, i, j)
+        cell[top] = T_ARROW
+        # The left score is the left neighbor plus the gap penalty.
+        left = self.best_gap_left(scores, i, j)
+        cell[left] = L_ARROW
+        # Final score is the min of the top-left, top, and left
+        # values.
+        final_score = min(top_left, top, left)
+        # The arrow points in the direction of the neighbor from where
+        # the best score came. This implementation doesn't include
+        # branches, but could with a little extra effort.
+        arrow = cell[final_score]
+        return (final_score, arrow)
+
+    def best_gap_top(self, scores: ScoreMatrix, i: int, j: int) -> int:
+        """Find the best gap score top of i, j."""
+        return min(scores[i-k][j] + self.gap_penalty(k) for k in range(1, i+1))
+
+    def best_gap_left(self, scores: ScoreMatrix, i: int, j: int) -> int:
+        """Find the best gap score left of i, j."""
+        return min(scores[i][j-k] + self.gap_penalty(k) for k in range(1, j+1))
+
+    def gap_penalty(self, k: int) -> int:
+        """Return affine gap penalty."""
+        return self.gap_start + k * self.gap_extend
+
+
+def nw_align(
         sequence1: str, sequence2: str, match: int=1, mismatch: int=-1, gap: int=-1,
     ) -> list[str]:
     """Return the pairwise alignment found using Needleman-Wunsch."""
-    scorer = SimilarityScorer(match, mismatch, gap)
-    (scores, arrows) = initialize_matrix(sequence1, sequence2, scorer)
+    scorer = NWScorer(match, mismatch, gap)
+    scores, arrows = initialize_matrix(sequence1, sequence2, scorer)
     path = list(trace_path(arrows))
     alignment = build_alignment(sequence1, sequence2, path)
     # score = compute_alignment_score(scores, path)
     return alignment
 
 
+def wsb_align(seq1: str, seq2: str, opts: Optional[ScoringOptions] = None) -> list[str]:
+    """Align two sequences using Smith-Waterman-Beyer."""
+    scorer = WSBScorer(opts)
+    scores, arrows = initialize_matrix(seq1, seq2, scorer)
+    path = list(trace_path(arrows))
+    alignment = build_alignment(seq1, seq2, path)
+    # score = compute_alignment_score(scores, path)
+    return alignment
+
+
 def initialize_matrix(
-        sequence1: str, sequence2: str, scorer: SimilarityScorer,
+        sequence1: str, sequence2: str, scorer: Scorer,
     ) -> tuple[ScoreMatrix, ArrowMatrix]:
     """Return the initialized matrix.
 
     Here's a small example:
 
     >>> scores, arrows = initialize_matrix(
-    ...    'at', 'aagt', SimilarityScorer(1, -1, -1))
+    ...    'at', 'aagt', NWScorer(1, -1, -1))
     >>> print_matrix(scores)
     [0, -1, -2, -3, -4]
     [-1, 1,  0, -1, -2]
@@ -150,10 +293,20 @@ def initialize_matrix(
     [↑ ↖ ← ← ←]
     [↑ ↑ ↖ ← ↖]
 
-    Here's a bigger example taken from the Needleman-Wunsch Wikipedia page:
+    >>> scores, arrows = initialize_matrix('at', 'aagt', WSBScorer())
+    >>> print_matrix(scores)
+    [0, 2, 3, 4, 5]
+    [2, 0, 2, 3, 4]
+    [3, 2, 1, 3, 3]
+    >>> print_arrow_matrix(arrows)
+    [∅ ← ← ← ←]
+    [↑ ↖ ← ← ←]
+    [↑ ↑ ↖ ← ↖]
+
+    More examples of Needleman-Wunsch:
 
     >>> scores, arrows = initialize_matrix(
-    ...     'gattaca', 'gcatgcu', SimilarityScorer(1, -1, -1))
+    ...     'gattaca', 'gcatgcu', NWScorer(1, -1, -1))
     >>> print_matrix(scores)
     [0,  -1, -2, -3, -4, -5, -6, -7]
     [-1,  1,  0, -1, -2, -3, -4, -5]
@@ -173,6 +326,21 @@ def initialize_matrix(
     [↑ ↑ ↖ ↑ ↑ ↑ ↖ ←]
     [↑ ↑ ↑ ↖ ← ↑ ↑ ↖]
 
+    >>> scores, arrows = initialize_matrix(
+    ...     'atgc', 'attgagc', NWScorer(1, -1, -1))
+    >>> print_matrix(scores)
+    [0,  -1, -2, -3, -4, -5, -6, -7]
+    [-1,  1,  0, -1, -2, -3, -4, -5]
+    [-2,  0,  2,  1,  0, -1, -2, -3]
+    [-3, -1,  1,  1,  2,  1,  0, -1]
+    [-4, -2,  0,  0,  1,  1,  0,  1]
+    >>> print_arrow_matrix(arrows)
+    [∅ ← ← ← ← ← ← ←]
+    [↑ ↖ ← ← ← ← ← ←]
+    [↑ ↑ ↖ ← ← ← ← ←]
+    [↑ ↑ ↑ ↖ ↖ ← ← ←]
+    [↑ ↑ ↑ ↑ ↑ ↖ ← ↖]
+
     """
     n = len(sequence1)
     m = len(sequence2)
@@ -190,21 +358,21 @@ def is_match(i: int, j: int, seq1: str, seq2: str) -> bool:
     return seq1[i-1] == seq2[j-1]
 
 
-def trace_path(matrix: ArrowMatrix) -> Iterable[tuple[int, int, int]]:
+def trace_path(arrows: ArrowMatrix) -> Iterable[tuple[int, int, int]]:
     """Trace the path back through the arrow matrix.
 
     Here's a small example:
 
     >>> scores, arrows = initialize_matrix(
-    ...     'at', 'aagt', SimilarityScorer(1, -1, -1))
+    ...     'at', 'aagt', NWScorer(1, -1, -1))
     >>> path = trace_path(arrows)
     >>> print(list(path))
     [(2, 4, 1), (1, 3, 3), (1, 2, 3), (1, 1, 1), (0, 0, 0)]
 
     """
-    i = len(matrix) - 1
-    j = len(matrix[0]) - 1
-    arrow = matrix[i][j]
+    i = len(arrows) - 1
+    j = len(arrows[0]) - 1
+    arrow = arrows[i][j]
     yield (i, j, arrow)
     while arrow != S_ARROW:
         if arrow == D_ARROW:
@@ -214,14 +382,14 @@ def trace_path(matrix: ArrowMatrix) -> Iterable[tuple[int, int, int]]:
             i -= 1
         elif arrow == L_ARROW:
             j -= 1
-        arrow = matrix[i][j]
+        arrow = arrows[i][j]
         yield (i, j, arrow)
 
 
 def build_alignment(sequence1: str, sequence2: str, path: Sequence[tuple[int, int, int]]) -> list[str]:
     """Align two sequences from the arrow path.
 
-    >>> _, arrows = initialize_matrix('at', 'aagt', SimilarityScorer(1, -1, -1))
+    >>> _, arrows = initialize_matrix('at', 'aagt', NWScorer(1, -1, -1))
     >>> path = list(trace_path(arrows))
     >>> alignment = build_alignment('at', 'aagt', path)
     >>> print_alignment(alignment)
@@ -258,8 +426,8 @@ def print_alignment(alignment: list[str]) -> None:
     """Print an alignment."""
     aligned1, aligned2 = alignment
     bars = ''
-    for i in range(len(aligned1)):
-        if aligned1[i] == aligned2[i]:
+    for (c1, c2) in zip(aligned1, aligned2):
+        if c1 == c2:
             bars += '|'
         else:
             bars += ' '
--- tests/align/pairwise.rst
+++ tests/align/pairwise.rst
@@ -1,22 +1,114 @@
-==================
-Pairwise Alignment
-==================
+========================
+Pairwise Alignment Tests
+========================
 
 Pairwise alignment algorithms attempt to find an optimal or
 near-optimal alignment between two sequences.
+
+These doctests exercise various components related to this
+implementation.
 
 
 Needleman-Wunsch
 ================
 
-    >>> from coolseq.align.pairwise import needleman_wunsch, print_alignment
-    >>> alignment = needleman_wunsch('at', 'aagt')
-    >>> print_alignment(alignment)
+    >>> from coolseq.align.pairwise import nw_align, print_alignment
+    >>> result = nw_align('at', 'aagt')
+    >>> print_alignment(result)
     a--t
     |  |
     aagt
-    >>> alignment = needleman_wunsch('gattaca', 'gcatgcu')
-    >>> print_alignment(alignment)
+    >>> result = nw_align('gattaca', 'gcatgcu')
+    >>> print_alignment(result)
     g-attaca
     | ||  |
     gcatg-cu
+    >>> result = nw_align('atgc', 'attgagc')
+    >>> print_alignment(result)
+    at-g--c
+    || |  |
+    attgagc
+
+
+Smith-Waterman-Beyer
+====================
+
+The WSBScorer encapsulates scoring logic for the Waterman-Smith-Beyer
+algorithm.
+
+    >>> from coolseq.align.pairwise import WSBScorer
+
+The WSB scorer sets a number of scoring options by default.
+
+    >>> scorer = WSBScorer()
+    >>> scorer.options
+    {'match': 0, 'mismatch': 1, 'gap_start': 1, 'gap_extend': 1}
+    >>> scorer.options == scorer.default_options
+    True
+
+Scoring options can be set when creating a scorer instance.
+
+    >>> scorer = WSBScorer({'gap_start': 5})
+    >>> scorer.options
+    {'match': 0, 'mismatch': 1, 'gap_start': 5, 'gap_extend': 1}
+    >>> scoring_options = {
+    ...     'match': 1,
+    ...     'mismatch': -1,
+    ...     'gap_start': 0,
+    ...     'gap_extend': -1,
+    ... }
+
+
+    >>> from coolseq.align.pairwise import (
+    ...     initialize_matrix,
+    ...     print_matrix,
+    ...     print_arrow_matrix,
+    ... )
+    >>> scores, arrows = initialize_matrix('ag', 'tagt', WSBScorer())
+    >>> print_matrix(scores)
+    [0, 2, 3, 4, 5]
+    [2, 1, 2, 4, 5]
+    [3, 3, 2, 2, 4]
+    >>> print_arrow_matrix(arrows)
+    [∅ ← ← ← ←]
+    [↑ ↖ ↖ ← ←]
+    [↑ ↑ ↖ ↖ ←]
+
+Put it all together.
+
+    >>> from coolseq.align.pairwise import wsb_align
+    >>> result = wsb_align('at', 'aagt')
+    >>> print_alignment(result)
+    a--t
+    |  |
+    aagt
+    >>> result = wsb_align('ag', 'tagt')
+    >>> print_alignment(result)
+    -ag-
+     ||
+    tagt
+    >>> result = wsb_align('gattaca', 'gcatgcu')
+    >>> print_alignment(result)
+    gattaca
+    |  | |
+    gcatgcu
+    >>> result = wsb_align('gattaca', 'gcatgcu', {'mismatch': 2})
+    >>> print_alignment(result)
+    g-attaca
+    | ||  |
+    gcatg-cu
+
+..    >>> print_alignment(result)
+..    a--t
+..    |  |
+..    aagt
+..    >>> result = wsb_align('gattaca', 'gcatgcu')
+..    >>> print_alignment(result)
+..    g-attaca
+..    | ||  |
+..    gcatg-cu
+..    >>> result = wsb_align('atgc', 'attgagc')
+..    >>> print_alignment(result)
+..    at-g--c
+..    || |  |
+..    attgagc
